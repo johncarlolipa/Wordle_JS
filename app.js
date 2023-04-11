3bfec510a990cb8da5efadd3ddc404b498a3b06d
JC~ <johncarlolipa@gmail.com>
2023-04-11 19:32:12 +0800

    added delete key function

---
+++ app.js
@@ -0,0 +1,117 @@
+const tileDisplay = document.querySelector(".tile-container");
+const keyboard = document.querySelector(".key-container");
+
+const wordle = "SUPER";
+
+const keys = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "ENTER",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "«",
+];
+
+const guessRows = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+let currentRow = 0;
+let currentTile = 0;
+
+//create div guessrow and also guessrow-tile div
+guessRows.forEach((guessRow, guessRowIndex) => {
+  const rowElement = document.createElement("div");
+  rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement("div");
+    tileElement.setAttribute(
+      "id",
+      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
+    );
+    tileElement.classList.add("tile");
+    rowElement.append(tileElement);
+  });
+  tileDisplay.append(rowElement);
+});
+
+//append keys to button
+keys.forEach((key) => {
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = key;
+  buttonElement.setAttribute("id", key);
+  buttonElement.addEventListener("click", () => handleClick(key));
+  keyboard.append(buttonElement);
+});
+
+//handlclick function; logging in the console the key that we click
+const handleClick = (key) => {
+  console.log("clicked", key);
+
+  if (key === "«") {
+    deleteKey();
+    console.log("guessRows", guessRows);
+    return;
+  }
+  if (key === "ENTER") {
+    console.log("check row");
+    console.log("guessRows", guessRows);
+    return;
+  }
+  addKey(key);
+  console.log("guessRows", guessRows);
+};
+
+//   each time we clicked a letter it is added to the correct tile
+
+const addKey = (key) => {
+  if (currentTile < 5 && currentRow < 6) {
+    const tile = document.getElementById(
+      "guessRow-" + currentRow + "-tile-" + currentTile
+    );
+    tile.textContent = key;
+    guessRows[currentRow][currentTile] = key;
+    tile.setAttribute("data", key);
+    currentTile++;
+  }
+};
+
+// function to delete key
+const deleteKey = () => {
+  if (currentTile > 0) {
+    currentTile--;
+    const tile = document.getElementById(
+      "guessRow-" + currentRow + "-tile-" + currentTile
+    );
+    tile.textContent = "";
+    guessRows[currentRow][currentTile] = ""
+    tile.setAttribute("data", "");
+  }
+};
